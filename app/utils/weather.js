be7e6af62956bed541eae7a888b2631203fb0b1a
shanery86 <shanery86@gmail.com>
2021-05-23 17:19:40 +0100

    fix

--- app/utils/weather.js
+++ app/utils/weather.js
@@ -6,8 +6,9 @@ if (result.error) {
   console.log(result.error.message);
   process.exit(1);
 }
-const apiKey = process.env.apiKey;
 */
+const apiKey = process.env.apiKey;
+
 const Weather = {
 
     getWeather: async function(lat,long) {
