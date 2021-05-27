da42057915004320fec7a36efe66dc522dfbac7a
shanery86 <shanery86@gmail.com>
2021-05-27 12:18:14 +0100

    fixes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const Vision = require("@hapi/vision");
 const Cookie = require("@hapi/cookie");
 const ImageStore = require('./app/utils/image-store');
 const User = require('./app/models/user');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 const handlebars = require('handlebars');
 const utils = require("./app/api/utils.js");
 
-
+/*
 const result = dotenv.config();
 if (result.error) {
   console.log(result.error.message);
